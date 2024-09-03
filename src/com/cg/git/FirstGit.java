b13dede20cef8e396b05413651bb8c1d4a94c727
RamuMylapalli <ramu.mylapalli@capgemini.com>
2024-09-03 15:17:32 +0530

    second commit

--- src/com/cg/git/FirstGit.java
+++ src/com/cg/git/FirstGit.java
@@ -3,6 +3,7 @@ package com.cg.git;
 public class FirstGit {
 	public static void main(String args[]) {
 		System.out.println("This is the first git");
+		System.out.println("Changing from git");
 	}
 
 }
